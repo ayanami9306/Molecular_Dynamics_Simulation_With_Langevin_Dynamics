8e64b4c70b0037afd80b4c580da7b57256303642
Nozomi <wlgnssla93@naver.com>
2018-03-05 18:49:15 +0900

    Mini MC Optimization
    Apply neighbor list about Mini Monte Carlo Simulation for initialization

--- MINI_MC.cpp
+++ MINI_MC.cpp
@@ -1,67 +1,80 @@
 #include "Molecular_Model.hpp"
-void Model_Segment::MINI_MC(int nCycle)
+int MC_LIST[20000][10000];
+int num_MCLIST[20000];
+void Model_Segment::MC_NEIGHBORLIST()
 {
-    for(int mccount=0; mccount<nCycle/2; mccount++)
+    double r2_Nebr = pow(rcut + radius_NebrShell, 2.0);
+    for(int i=0; i<nParticle;i++) num_MCLIST[i] = 0;
+    for(int i=0; i<nParticle-1; i++)
     {
-        for(int i=0; i<nParticle; i++)
+        for(int j=i+1; j<nParticle; j++)
         {
-            int rSegment = (int)(((double)rand()/RAND_MAX)*nParticle);
-            double theta = 2*PI*((double)rand()/RAND_MAX), phi = 2*PI*((double)rand()/RAND_MAX), radius = rMax*((double)rand()/RAND_MAX);
-            double temp_coordinate[3] =
+            double temp[3];
+            double dist2 = Get_Distance2(i, j, temp);
+            if(dist2 < r2_Nebr)
             {
-                Segment[rSegment].coordinate[0],
-                Segment[rSegment].coordinate[1],
-                Segment[rSegment].coordinate[2]
-            };
-            Segment[rSegment].coordinate[0] += radius * sin(theta) * cos(phi);
-            Segment[rSegment].coordinate[1] += radius * sin(theta) * sin(phi);
-            Segment[rSegment].coordinate[2] += radius * cos(theta);
-            
-            bool check = true;
-            for(int j=0; j<Segment[rSegment].linked_segment_num;j++)
-            {
-                double pair_distnace[3];
-                double num2 = Segment[rSegment].linked_segment[j];
-                double distance = sqrt(Get_Distance2(rSegment, num2, pair_distnace));
-                if(distance > 1.02*bond_length_FENE_0 * 0.67 || distance < 0.98*bond_length_FENE_0 * 0.67) check = false;
-            }
-            if(!check)
-            {
-                Segment[rSegment].coordinate[0] = temp_coordinate[0];
-                Segment[rSegment].coordinate[1] = temp_coordinate[1];
-                Segment[rSegment].coordinate[2] = temp_coordinate[2];
+                MC_LIST[i][num_MCLIST[i]++] = j;
+                MC_LIST[j][num_MCLIST[j]++] = i;
             }
         }
-        if(mccount%step_AVG == 0) Mol2_File_Write(false);
     }
-    for(int mccount=0; mccount<nCycle/2; mccount++)
+}
+void Model_Segment::MINI_MC(int nCycle)
+{
+    MC_NEIGHBORLIST();
+    int BUILD_COUNT = (int)(radius_NebrShell/(rMax));
+    for(int mccount=0; mccount<nCycle; mccount++)
     {
+        if(mccount % BUILD_COUNT) MC_NEIGHBORLIST();
         for(int i=0; i<nParticle; i++)
         {
+            //calc before potential
             double pot_pre = 0, pot_after = 0;
             int rSegment = (int)(((double)rand()/RAND_MAX)*nParticle);
-            for(int j=0;j<nParticle;j++)
+            for(int j=0;j<num_MCLIST[rSegment];j++)
             {
-                if(j!=rSegment)
+                int num2 = MC_LIST[rSegment][j];
+                double pair_distance[3];
+                double distance2 = Get_Distance2(rSegment, num2, pair_distance);
+                if(distance2 <= rcut2)
                 {
-                    double pair_distance[3];
-                    double distance2 = Get_Distance2(rSegment, j, pair_distance);
-                    if(distance2 <= rcut2)
-                    {
-                        double inv_distance2 = 1.0 / distance2;
-                        double inv_distance6 = pow(inv_distance2, 3.0);
-                        pot_pre += 4 * epsilon * inv_distance6 * (inv_distance6 - 1.0) - potential_rcut;
-                    }
+                    double inv_distance2 = 1.0 / distance2;
+                    double inv_distance6 = pow(inv_distance2, 3.0);
+                    pot_pre += 4 * epsilon * inv_distance6 * (inv_distance6 - 1.0) - potential_rcut;
                 }
             }
             for(int j=0; j<Segment[rSegment].linked_segment_num;j++)
             {
-                int num2 = Segment[rSegment].linked_segment[j];
-                double bond_distance[3];
-                double distance = sqrt(Get_Distance2(rSegment, num2, bond_distance));
-                if(distance < bond_length_FENE_0) pot_pre += -0.5*k_FENE*pow(bond_length_FENE_0,2.0)*log(1-pow(distance/bond_length_FENE_0, 2.0));
+                int num_prev = Segment[rSegment].linked_segment[j];
+                double bond_prev[3], bond_next[3];
+                //ba vector
+                double distance_prev = sqrt(Get_Distance2(num_prev, rSegment, bond_prev));
+                if(distance_prev < bond_length_FENE_0) pot_pre += -0.5*k_FENE*pow(bond_length_FENE_0,2.0)*log(1-pow(distance_prev/bond_length_FENE_0, 2.0));
                 else pot_pre+=10E99;
+#if APPLY_BOND_ANGLE
+                if(Segment[rSegment].segment_type)
+                {
+                    for(int k=j+1; k<Segment[rSegment].linked_segment_num; k++)
+                    {
+                        int num_next = Segment[rSegment].linked_segment[k];
+                        if(Segment[num_next].segment_type || Segment[num_prev].segment_type)
+                        {
+                            //bc vector
+                            double distance_next = sqrt(Get_Distance2(num_next, rSegment, bond_next));
+                            
+                            double Cos_Value = scalar_multiply(bond_prev, bond_next) / (distance_prev * distance_next);
+                            double bond_angle;
+                            if(Cos_Value >= 1) bond_angle = 0;
+                            else if(Cos_Value <= -1) bond_angle = PI;
+                            else bond_angle = acos(Cos_Value);
+                            pot_pre += bond_angle_k * pow(bond_angle-bond_angle_0, 2.0);
+                        }
+                    }
+                }
+#endif
             }
+            
+            //calc after potential
             double theta = 2*PI*((double)rand()/RAND_MAX), phi = 2*PI*((double)rand()/RAND_MAX), radius = rMax*((double)rand()/RAND_MAX);
             double temp_coordinate[3] =
             {
@@ -73,27 +86,47 @@ void Model_Segment::MINI_MC(int nCycle)
             Segment[rSegment].coordinate[1] += radius * sin(theta) * sin(phi);
             Segment[rSegment].coordinate[2] += radius * cos(theta);
             
-            for(int j=0;j<nParticle;j++)
+            for(int j=0;j<num_MCLIST[rSegment];j++)
             {
-                if(j!=rSegment)
+                int num2 = MC_LIST[rSegment][j];
+                double pair_distance[3];
+                double distance2 = Get_Distance2(rSegment, num2, pair_distance);
+                if(distance2 <= rcut2)
                 {
-                    double pair_distance[3];
-                    double distance2 = Get_Distance2(rSegment, j, pair_distance);
-                    if(distance2 <= rcut2)
-                    {
-                        double inv_distance2 = 1.0 / distance2;
-                        double inv_distance6 = pow(inv_distance2, 3.0);
-                        pot_after += 4 * epsilon * inv_distance6 * (inv_distance6 - 1.0) - potential_rcut;
-                    }
+                    double inv_distance2 = 1.0 / distance2;
+                    double inv_distance6 = pow(inv_distance2, 3.0);
+                    pot_after += 4 * epsilon * inv_distance6 * (inv_distance6 - 1.0) - potential_rcut;
                 }
             }
             for(int j=0; j<Segment[rSegment].linked_segment_num;j++)
             {
-                int num2 = Segment[rSegment].linked_segment[j];
-                double bond_distance[3];
-                double distance = sqrt(Get_Distance2(rSegment, num2, bond_distance));
-                if(distance < bond_length_FENE_0) pot_after += -0.5*k_FENE*pow(bond_length_FENE_0,2.0)*log(1-pow(distance/bond_length_FENE_0, 2.0));
+                int num_prev = Segment[rSegment].linked_segment[j];
+                double bond_prev[3], bond_next[3];
+                //ba vector
+                double distance_prev = sqrt(Get_Distance2(num_prev, rSegment, bond_prev));
+                if(distance_prev < bond_length_FENE_0) pot_after += -0.5*k_FENE*pow(bond_length_FENE_0,2.0)*log(1-pow(distance_prev/bond_length_FENE_0, 2.0));
                 else pot_after+=10E99;
+#if APPLY_BOND_ANGLE
+                if(Segment[rSegment].segment_type)
+                {
+                    for(int k=j+1; k<Segment[rSegment].linked_segment_num; k++)
+                    {
+                        int num_next = Segment[rSegment].linked_segment[k];
+                        if(Segment[num_next].segment_type || Segment[num_prev].segment_type)
+                        {
+                            //bc vector
+                            double distance_next = sqrt(Get_Distance2(num_next, rSegment, bond_next));
+                            
+                            double Cos_Value = scalar_multiply(bond_prev, bond_next) / (distance_prev * distance_next);
+                            double bond_angle;
+                            if(Cos_Value >= 1) bond_angle = 0;
+                            else if(Cos_Value <= -1) bond_angle = PI;
+                            else bond_angle = acos(Cos_Value);
+                            pot_after += bond_angle_k * pow(bond_angle-bond_angle_0, 2.0);
+                        }
+                    }
+                }
+#endif
             }
             
             double delta_pot = pot_after - pot_pre;
--- Molecular_Model.hpp
+++ Molecular_Model.hpp
@@ -15,10 +15,12 @@
 //#include "omp.h"
 #include "WELL512a.hpp"
 
-#define NUM_MOLECULE 20000
+#define NUM_MOLECULE 100000
 #define MAX_CORENUM 30
 #define PI 3.141592
 
+#define scalar_multiply(a, b) (a[0]*b[0] + a[1]*b[1] + a[2]*b[2])
+#define APPLY_BOND_ANGLE 1
 using namespace std;
 
 #endif /* Molecular_Model_hpp */
@@ -40,8 +42,9 @@ private:
     Node Segment[NUM_MOLECULE];
     int nParticle, step_AVG, Limit_Cycle, BOUNDARY_SIZE_X, BOUNDARY_SIZE_Y, BOUNDARY_SIZE_Z, pragma_core, dp_dendron, generation_dendron, number_branch, num_NebrList, space_sidechain;
     double deltaT, rcut, segment_mass, zeta, rand_deviation, epsilon, rMax, inv_nParticle, inv_RAND_MAX;
-    double deltaT_half, deltaT2_half, rcut2, inv_step_AVG, potential_rcut, kT_0, k_FENE, vvMax, vCM_Total, pot_step, kin_step, time_Now, inv_Rand_Max, accu_movement, radius_NebrShell, bond_length_FENE_0, Lp[10000], inv_segment_mass, bond_length;
-    unsigned int NebrList_1[50000000], NebrList_2[50000000];
+    double deltaT_half, deltaT2_half, rcut2, inv_step_AVG, potential_rcut, kT_0, k_FENE, vvMax, vCM_Total, pot_step, kin_step, time_Now, inv_Rand_Max, accu_movement, radius_NebrShell, bond_length_FENE_0, Lp[10000], inv_segment_mass, bond_length, bond_angle_0, bond_angle_k;
+    double Rx_backbone, Ry_backbone, Rz_backbone, Rg_backbone, Rx_sidechain, Ry_sidechain, Rz_sidechain, Rg_sidechain, Rx_total, Ry_total, Rz_total, Rg_total;
+    unsigned int NebrList_1[100000000], NebrList_2[100000000];
     bool recon_Nebrlist;
     char write_filename[100], dat_filename[100], traj_filename[100], Lp_filename[100], Rg_filename[100];
     
@@ -73,8 +76,8 @@ private:
     void Periodic_Boundary (double * data);
     void Velocity_Verlet_Step();
     void Velocity_Verlet_After_Step();
-    double Calc_Lennard_Jones_Potential();
-    double Calc_Bond_Length_Potential_AND_Apply_Langevin();
+    double Calc_Nonbonded_Potential();
+    double Calc_Bonded_Potential_AND_Apply_Langevin();
     void recursive_branch(int parent, int generation);
     void Evaluate_Properties();
     void PDB_File_Write(bool is_new_file);
@@ -82,6 +85,11 @@ private:
     void Mol2_File_Read(char *filename);
     void MINI_MC(int nCycle);
     void Measure_Persistence_Length();
-    double Measure_Radius_of_Gyration();
+    void Measure_Radius_of_Gyration();
     double Measure_Rotational_Viscosity();
+    void Vector_Cross(double *V1, double *V2, double *VR);
+    void Vector_Constant(double *V1, double constant, double*VR);
+    void Normalize_Vector(double *Vector);
+    void Print_LAMMPS();
+    void MC_NEIGHBORLIST();
 };
